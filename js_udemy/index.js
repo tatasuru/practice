4bad02ee96d4ed5190b292c90728c6b138ce181f
tatsu <k.t.19963469@gmail.com>
2022-05-11 23:52:33 +0900

    [add] 2022/05/11 連想配列など

---
+++ js_udemy/index.js
@@ -0,0 +1,27 @@
+const member = {
+  //連想配列
+  "name":"honda",
+  "height" :100
+}
+
+console.log(member.name)
+
+
+const member_02 = {
+  //連想配列
+  "honda":{
+    "height":170
+  },
+  "kagawa":{
+    "height":160
+  }
+}
+
+console.log(member_02);
+
+
+
+const test_01 = 4;
+const test_02 = 4;
+const test_03 = test_01 - test_02;
+console.log(test_03)
