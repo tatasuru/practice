1eb2059eb0be76bb850c47a4d0e11631b279e53c
tatsu <k.t.19963469@gmail.com>
2022-05-18 23:48:19 +0900

    [add] 2022/05/17

--- js_udemy/index.js
+++ js_udemy/index.js
@@ -1,27 +1,20 @@
-const member = {
-  //連想配列
-  "name":"honda",
-  "height" :100
-}
+// const formTestName = document.forms.test1.yourName;
+// console.log(formTestName);
 
-console.log(member.name)
+// formTestName.addEventListener("input",() => {
+//   let youtNameValue = document.getElementById("yout_name_tag");
+//   youtNameValue.textContent = formTestName.value.length;
+// });
 
+const imgTag = document.forms.test1.preview
+document.forms.test1.img_file.addEventListener("change", e => {
+  const upFile = e.target.files[0];
 
-const member_02 = {
-  //連想配列
-  "honda":{
-    "height":170
-  },
-  "kagawa":{
-    "height":160
-  }
-}
-
-console.log(member_02);
+  const reader  = new FileReader();
+  reader.addEventListener("load", () => {
+    imgTag.src = reader.result;
+  });
+  reader.readAsDataURL(upFile);
+});
 
 
-
-const test_01 = 4;
-const test_02 = 4;
-const test_03 = test_01 - test_02;
-console.log(test_03)
---
+++ js_udemy/index_02.js
@@ -0,0 +1,31 @@
+
+
+// window.addEventListener("scroll",() => {
+//   let windowY = window.scrollY;
+//   console.log(windowY)
+// })
+
+ const options ={
+  //  root:,
+  // threshold:0.5,
+  rootMargin: "0px 0px -50% 0px",
+ };
+ const observer =  new IntersectionObserver(callback,options)
+ const targets = document.querySelectorAll(".message-box");
+//  console.log(targets);
+
+ targets.forEach( (target) => {
+   //targets配列の中の１つ１つを監視していく
+   observer.observe(target)
+  });
+
+
+  //observerの中で呼び出されるcallback関数
+ function callback (entries){
+   console.log(entries);//配列
+   entries.forEach( entry => {
+     if(entry.isIntersecting){
+       entry.target.classList.add("show");
+     }
+   })
+ }
