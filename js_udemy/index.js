7be85fe0354c651ea2e485338382e9c963a9e27a
tatsu <k.t.19963469@gmail.com>
2022-06-05 10:35:02 +0900

    [add] js コードマフィア

--- js_udemy/index.js
+++ js_udemy/index.js
@@ -1,20 +1,20 @@
-// const formTestName = document.forms.test1.yourName;
-// console.log(formTestName);
+// // const formTestName = document.forms.test1.yourName;
+// // console.log(formTestName);
 
-// formTestName.addEventListener("input",() => {
-//   let youtNameValue = document.getElementById("yout_name_tag");
-//   youtNameValue.textContent = formTestName.value.length;
+// // formTestName.addEventListener("input",() => {
+// //   let youtNameValue = document.getElementById("yout_name_tag");
+// //   youtNameValue.textContent = formTestName.value.length;
+// // });
+
+// const imgTag = document.forms.test1.preview
+// document.forms.test1.img_file.addEventListener("change", e => {
+//   const upFile = e.target.files[0];
+
+//   const reader  = new FileReader();
+//   reader.addEventListener("load", () => {
+//     imgTag.src = reader.result;
+//   });
+//   reader.readAsDataURL(upFile);
 // });
-
-const imgTag = document.forms.test1.preview
-document.forms.test1.img_file.addEventListener("change", e => {
-  const upFile = e.target.files[0];
-
-  const reader  = new FileReader();
-  reader.addEventListener("load", () => {
-    imgTag.src = reader.result;
-  });
-  reader.readAsDataURL(upFile);
-});
 
 
--- js_udemy/promise.js
+++ js_udemy/promise.js
@@ -1,62 +1,62 @@
-function test01(input) {
-  return new Promise((ok) => {
-    ok(`プロミス${input}です！`);
-  });
-}
+// function test01(input) {
+//   return new Promise((ok) => {
+//     ok(`プロミス${input}です！`);
+//   });
+// }
 
-console.log(test01("太郎"));
-
-
+// console.log(test01("太郎"));
 
 
 
-console.log(1);
 
 
-promisetest4(400,2)
-.then(change1 => promisetest4(change1,3))
-.then(change2 => promisetest4(change2,2))
-.then(change3 => promisetest4(change3,2))
-.catch( e =>  console.log(e) )
-
-console.log(3);
+// console.log(1);
 
 
+// promisetest4(400,2)
+// .then(change1 => promisetest4(change1,3))
+// .then(change2 => promisetest4(change2,2))
+// .then(change3 => promisetest4(change3,2))
+// .catch( e =>  console.log(e) )
 
-function promisetest4 (pay,seconds) {
-  return new Promise((ok,ng) => {
-    setTimeout(() => {
-      if(pay > 100){
-        let change = pay - 100;
-        console.log(`${seconds}秒、お釣りは${change}です！`);
-        ok(change);
-      }
-      ng("お金が足りません！");
-    }, seconds * 1000)
-  })
-}
-
-// console.log(promisetest4(180).catch( e => {
-//   console.log(e);
-// }));
+// console.log(3);
 
 
 
-// promisetest4(180).then( change1 => {
-//   console.log(change1);
-//   return promisetest4(change1); //100
-// }).then(change2 => {
-//   console.log(change2);//100
-// }).catch(error => {
-//   console.log(error);
-// })
+// function promisetest4 (pay,seconds) {
+//   return new Promise((ok,ng) => {
+//     setTimeout(() => {
+//       if(pay > 100){
+//         let change = pay - 100;
+//         console.log(`${seconds}秒、お釣りは${change}です！`);
+//         ok(change);
+//       }
+//       ng("お金が足りません！");
+//     }, seconds * 1000)
+//   })
+// }
+
+// // console.log(promisetest4(180).catch( e => {
+// //   console.log(e);
+// // }));
 
 
-const parentDiv = document.getElementById("img_wrapper");
-let newEle = document.createElement("p");
-let newTxt = document.createTextNode("iiiii");
-newEle.appendChild(newTxt);
-newEle.setAttribute("id","ele_02");
-let child_03 = document.getElementById("ele_03");
 
-parentDiv.insertBefore(newEle,child_03);
+// // promisetest4(180).then( change1 => {
+// //   console.log(change1);
+// //   return promisetest4(change1); //100
+// // }).then(change2 => {
+// //   console.log(change2);//100
+// // }).catch(error => {
+// //   console.log(error);
+// // })
+
+
+// const parentDiv = document.getElementById("img_wrapper");
+// let newEle = document.createElement("p");
+// let newTxt = document.createTextNode("iiiii");
+// newEle.appendChild(newTxt);
+// newEle.setAttribute("id","ele_02");
+// let child_03 = document.getElementById("ele_03");
+
+// parentDiv.insertBefore(newEle,child_03);
